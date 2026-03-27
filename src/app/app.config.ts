473882fe8b0c2e7a5fabf8b9bbb5044577ee8827
DevanshRajani25 <devanshrajani25@gmail.com>
2026-03-27 09:00:26 +0530

    feat: toastr added for popups and connected basic backend with API call

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,6 +10,15 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-top-right',
+      timeOut: 3000,
+      progressBar: true,
+      closeButton: true,
+      preventDuplicates: true
+    })
   ]
 };
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -18,39 +20,57 @@ export class Login {
   showForgotPassword: boolean = false;
   otpUsername: string = '';
 
-  private readonly emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  constructor(private http: HttpClient, private toastr: ToastrService) {}
+
+  private readonly emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   onLogin() {
 
     this.username = this.username.trim();
 
     if (!this.username && !this.password) {
-      alert('Please fill in all the details!');
+      this.toastr.warning('Please fill in all the details!');
       return;
     }
     else if (!this.username){
-      alert('Please enter username!');
+      this.toastr.warning('Please enter username!');
       return;
     }
     else if (!this.emailRegex.test(this.username)) {
-      alert('Please enter a valid username!');
+      this.toastr.warning('Please enter a valid username!');
       return;
     }
     else if (!this.password) {
-      alert('Please enter Password!');
+      this.toastr.warning('Please enter Password!');
       return;
     }
 
+    // Request Payload
     const userData = {
       email: this.username,
       password: this.password
     };
-    
-    console.log('User Data:', userData);
-    alert('Login successful!');
 
-    this.username = '';
-    this.password = '';
+    this.http.post('http://localhost:8081/auth/login', userData, { responseType: 'text' })
+      .subscribe({
+        next: (res) => {
+          console.log("Response:", res);
+          this.toastr.success(res || 'Login successful!');
+          this.username = '';
+          this.password = '';
+        },
+        error: (err: HttpErrorResponse) => {
+          console.error(err);
+          if (err.status === 0) {
+            this.toastr.error('Internal Server Error.');
+            return;
+          }
+
+          this.toastr.error(err?.error || 'Login failed !');
+        }
+      });
+
+    console.log('User Data:', userData);
   }
 
   togglePassword() {
@@ -67,16 +87,16 @@ export class Login {
     this.otpUsername = this.otpUsername.trim();
 
     if (!this.otpUsername) {
-      alert('Please enter your email!');
+      this.toastr.warning('Please enter your email!');
       return;
     }
     else if (!this.emailRegex.test(this.otpUsername)) {
-      alert('Please enter a valid email address!');
+      this.toastr.warning('Please enter a valid email address!');
       return;
     }
 
     console.log('OTP sent to:', this.otpUsername);
-    alert('OTP has been sent to your email!');
+    this.toastr.success('OTP has been sent to your email!', 'Success');
     this.otpUsername = '';
     this.showForgotPassword = false;
   }
